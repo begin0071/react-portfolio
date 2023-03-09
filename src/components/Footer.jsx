9c8d4187864703188939564f6ac5905187b58bcf
begin0071 <m.khaki610@gmail.com>
2023-03-09 16:18:16 -0600

    links tofooter

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Footer(){
+
+  return(
+    <footer>
+      <div>
+        <a href="https://github.com/begin0071"><span>Github</span></a>
+        <a href="https://www.linkedin.com/in/mugeeskhaki/"><span>LinkedIn</span></a>
+      </div>
+    </footer>
+
+  )
+}
+
+export default Footer
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+const Navigation = ({current, setCurrent}) => {
+  return (
+    <div>
+      <nav class = "header">
+        <h1>Christopher Clark</h1>
+        <div className = "menu">
+          <p className = "navItem" onClick={()=>setCurrent("about me")}>About</p>
+          <p className = "navItem" onClick={()=>setCurrent("portfolio")}>Portfolio</p>
+          <p className = "navItem" onClick={()=>setCurrent("contact me")}>Contact</p>
+          <p className = "navItem" onClick={()=>setCurrent("resume")}>Resume</p>
+        </div>
+      </nav>
+      
+    </div>
+
+
+
+  )
+}
+
+export default Navigation
+
+
